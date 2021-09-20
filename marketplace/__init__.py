fe1a36bcdb521c6a5591e3b2e1000f0338ab2f6b
Richard Neumann <mail@richard-neumann.de>
2021-09-20 12:15:58 +0200

    Add ORM related functions

--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -1,6 +1,21 @@
 """Market place library."""
 
+from marketplace.functions import get_offers
+from marketplace.functions import get_offer
+from marketplace.functions import add_offer
+from marketplace.functions import get_images
+from marketplace.functions import get_image
+from marketplace.functions import add_image
 from marketplace.orm import Offer, Image
 
 
-__all__ = ['Offer', 'Image']
+__all__ = [
+    'get_offers',
+    'get_offer',
+    'add_offer',
+    'get_images',
+    'get_image',
+    'add_image',
+    'Offer',
+    'Image'
+]
---
+++ marketplace/functions.py
@@ -0,0 +1,59 @@
+"""Common functions."""
+
+from peewee import ModelSelect
+
+from comcatlib import User
+from filedb import File
+
+from marketplace.orm import Offer, Image
+
+
+__all__ = [
+    'get_offers',
+    'get_offer',
+    'add_offer',
+    'get_images',
+    'get_image',
+    'add_image'
+]
+
+
+def get_offers(user: User) -> ModelSelect:
+    """Yields the user's offers."""
+
+    return Offer.select().where(Offer.user == user)
+
+
+def get_offer(ident: int, user: User) -> Offer:
+    """Returns the given offer."""
+
+    return get_offers(user).where(Offer.id == ident).get()
+
+
+def add_offer(json: dict, user: User) -> Offer:
+    """Adds a new offer."""
+
+    offer = Offer.from_json(json)
+    offer.user = user
+    offer.save()
+    return offer
+
+
+def get_images(offer: Offer) -> ModelSelect:
+    """Yields the offer's images."""
+
+    return offer.images
+
+
+def get_image(ident: int, offer: Offer) -> Image:
+    """Returns the given image."""
+
+    return get_images(offer).where(Image.id == ident).get()
+
+
+def add_image(offer: Offer, image: bytes, *, index: int = 0) -> Image:
+    """Adds an image attachment to an offer."""
+
+    image = Image(offer=offer, file=File.from_bytes(image), index=index)
+    image.save()
+    return image
