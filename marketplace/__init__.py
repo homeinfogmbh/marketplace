ceca785d1f5a11e564ae453946aec6e3420b95d8
Richard Neumann <mail@richard-neumann.de>
2021-09-21 15:43:30 +0200

    Add further exceptions

--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -1,6 +1,6 @@
 """Market place library."""
 
-from marketplace.exceptions import InvalidPrice
+from marketplace.exceptions import ImageTooLarge, InvalidPrice, MaxImagesReached
 from marketplace.functions import get_offers
 from marketplace.functions import get_offer
 from marketplace.functions import add_offer
@@ -10,7 +10,9 @@ from marketplace.orm import Offer, Image
 
 
 __all__ = [
+    'ImageTooLarge',
     'InvalidPrice',
+    'MaxImagesReached',
     'get_offers',
     'get_offer',
     'add_offer',
