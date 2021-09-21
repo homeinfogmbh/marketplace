9b922b347e6b34f4ec4b8ac3bceea56195cb1ada
Richard Neumann <mail@richard-neumann.de>
2021-09-21 12:48:31 +0200

    Fix key name

--- marketplace/config.py
+++ marketplace/config.py
@@ -11,4 +11,4 @@ MAX_IMAGES = CONFIG.getint('limits', 'max_images', fallback=4)
 THREE_MB = 3 * 1024 * 1024
 MAX_IMAGE_SIZE = CONFIG.getint('limits', 'max_image_size', fallback=THREE_MB)
 MAX_PRICE = CONFIG.getint('limits', 'max_price', fallback=9999)
-MIN_PRICE = CONFIG.getint('limits', 'max_price', fallback=0)
+MIN_PRICE = CONFIG.getint('limits', 'min_price', fallback=0)
