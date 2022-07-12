05118c912fc8e91f9043b669521c60c68499d6b9
Richard Neumann <r.neumann@homeinfo.de>
2022-07-12 09:55:11 +0200

    Update indentation

--- marketplace/errors.py
+++ marketplace/errors.py
@@ -15,14 +15,22 @@ __all__ = ['ERRORS']
 ERRORS = {
     Image.DoesNotExist: lambda _: JSONMessage('No such image.', status=404),
     ImageTooLarge: lambda error: JSONMessage(
-        'Image too large.', size=error.size, max=error.max_size, status=400),
+        'Image too large.', size=error.size, max=error.max_size, status=400
+    ),
     InvalidPrice: lambda error: JSONMessage(
-        'Invalid price.', value=error.value, min=error.min_price,
-        max=error.max_price, status=400),
+        'Invalid price.',
+        value=error.value,
+        min=error.min_price,
+        max=error.max_price,
+        status=400
+    ),
     MaxImagesReached: lambda error: JSONMessage(
-        'Maximum amount of images reached.', max=error.max_images, status=400),
+        'Maximum amount of images reached.', max=error.max_images, status=400
+    ),
     MissingContactInfo: lambda _: JSONMessage(
         'Missing contact info.',
-        hint='Must either specify email or phone number.', status=400),
+        hint='Must either specify email or phone number.',
+        status=400
+    ),
     Offer.DoesNotExist: lambda _: JSONMessage('No such offer.', status=404),
 }
